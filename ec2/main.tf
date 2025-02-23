c6d045eaaec5d8260b118af4721dec42da6a32b8
root <root@ip-10-0-15-174.ap-south-1.compute.internal>
2025-02-23 18:40:45 +0000

    added main.tf and provider.tf

---
+++ ec2/main.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "example_sg" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ExampleSecurityGroup"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.example_sg.id]
+
+  tags = {
+    Name = var.instance_name
+  }
+}
